41be74fbacf872d444dda102cbb9c08ac5382322
loalex523 <loalex523@gmail.com>
2020-06-15 17:24:25 -0500

    6/15/2020 - 5:24
    Updated all links.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Title from './Components/Title'
 import Navbar from './Components/Navbar'
-
+import About from './Components/Pages/About'
+import Contact from './Components/Pages/Contact'
+import Projects from './Components/Pages/Projects'
 //import { Contact } from './Components/Pages/Contact'
 function App() {
   return (
+    <Router>
     <div className='main-container'>
       <div className='title-container'>
         <Title className='title-container'/>
       </div>
       <div>
         <Navbar className='navbar-container'/>
+        <Switch>
+            <Route path="/projects" component={ Projects }/>  
+            <Route path="/about" component={ About }/>
+            <Route path="/contact" component={ Contact }/>
+        </Switch>
       </div>
     </div>
+    </Router>
+    
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,23 @@
 import React from 'react'
 import './Components.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { motion } from "framer-motion"
-import About from './Pages/About'
-import Contact from './Pages/Contact'
 
 
 function Navbar(){
     return(
-        <Router>
-            <div className='navbar-container'>
-                <motion.button whileHover={{opacity: 0.3}} whileTap={{scale: 0.9}} className='navButton' exact to='/' activeClassName="active"> Projects </motion.button>
-                <Switch>
-                    <Route path="/about" component={ About }/>
-                    <Route path="/contact" component={ Contact }/>
-                </Switch>
-                
-            </div>
-        </Router>
-        
+        <div className='navbar-container'>
+            <Link to="./projects">
+                <motion.button whileHover={{opacity: 0.3}} whileTap={{scale: 0.9}} className='navButton' exact to='/' activeClassName="active"> Projects </motion.button>  
+            </Link>
+            <Link to="./about">
+                <motion.button whileHover={{opacity: 0.3}} whileTap={{scale: 0.9}} className='navButton'> About Me </motion.button>
+            </Link>
+            <Link to="./contact">
+                <motion.button whileHover={{opacity: 0.3}} whileTap={{scale: 0.9}} className='navButton'> Contact </motion.button>            
+            </Link>
+        </div>      
     )
 }
 export default Navbar;
 
-{/* <motion.button whileHover={{opacity: 0.3}} whileTap={{scale: 0.9}} className='navButton'> About Me </motion.button>
-           <motion.button whileHover={{opacity: 0.3}} whileTap={{scale: 0.9}} className='navButton'> Contact </motion.button> */}
