df0f34602d2bc8322a14c4177ec8e736d87f1b75
loalex523 <loalex523@gmail.com>
2020-12-21 18:57:03 -0600

    Added Relevant Animations
    12/21/2020

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,14 +9,14 @@ function Footer(){
 
     return(
         <div className='footer'>
-            <div className='footer-text'> Let's build something together! </div>
+            {/* <div className='footer-text'> Let's build something together! </div> */}
             <div className='footer-socials'> 
-            <Socials url='https://github.com/loalex523' image={git} size='footer-size'/>
-                    <Socials url='https://www.linkedin.com/in/alex-j-lo/' image={linkedin} size='footer-size'/>
-                    <Socials url='https://www.instagram.com/alexjlo_/?hl=en' image={insta} size='footer-size'/>
-                    <Socials url='mailto:loalex523@gmail.com' image={email} size='footer-size'/>
+                <Socials url='https://github.com/loalex523' image={git} size='footer-size'/>
+                <Socials url='https://www.linkedin.com/in/alex-j-lo/' image={linkedin} size='footer-size'/>
+                <Socials url='https://www.instagram.com/alexjlo_/?hl=en' image={insta} size='footer-size'/>
+                <Socials url='mailto:loalex523@gmail.com' image={email} size='footer-size'/>
             </div>
-            <div className='footer-bottom'> Made by Alex Lo </div>
+            <div className='footer-bottom'> Made w/ ❤️ by Alex Lo </div>
         </div>
     )
 }
--- src/Components/Socials.js
+++ src/Components/Socials.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Components.css'
+import { motion } from "framer-motion"
 
 function Socials(props){
     return(
-        <div>
-            <a href={props.url}>
-                <img alt = "logo192.png" src={props.image} className={props.size}></img>
-            </a>
-        </div>
+            <motion.a href={props.url} whileTap={{ scale: 0.9}} whileHover={{ scale: 1.2 }}>
+                <img alt = "logo192.png" src={props.image} className={props.size} ></img>
+            </motion.a>
     )
 }
 export default Socials;
